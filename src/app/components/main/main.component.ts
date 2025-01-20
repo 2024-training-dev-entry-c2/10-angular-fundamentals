d92c578e266e4914405629d67a951f9380058ef5
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2025-01-20 12:07:46 -0300

    feature (ts) add-animations

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { projects } from './projects-data';
 import { MainIntroComponent } from '../main-intro/main-intro.component';
 import { MainProjectComponent } from '../main-project/main-project.component';
@@ -16,6 +16,45 @@ import { MainSeparatorComponent } from '../main-separator/main-separator.compone
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit {
   projects = projects;
+
+  ngAfterViewInit(): void {
+    const ctaElements = document.querySelectorAll(
+      '.duties__project-cta, .duties__about-cta'
+    );
+
+    ctaElements.forEach((cta) => {
+      const icon = cta.querySelector(
+        '.duties__project-cta-icon, .duties__about-cta-icon'
+      ) as SVGElement;
+      const clone = icon.cloneNode(true) as SVGElement;
+      clone.classList.add(
+        'duties__project-cta-icon-clone',
+        'duties__about-cta-icon-clone'
+      );
+      cta.appendChild(clone);
+
+      let originalTransform = icon.style.transform;
+
+      cta.addEventListener('mouseenter', () => {
+        originalTransform = icon.style.transform;
+        icon.style.animation = 'moveRight 0.5s forwards';
+        clone.style.animation = 'appear 0.5s forwards';
+      });
+
+      cta.addEventListener('mouseleave', () => {
+        icon.style.animation = 'moveLeft 0.5s forwards';
+        clone.style.animation = 'disappear 0.5s forwards';
+      });
+
+      icon.addEventListener('animationend', () => {
+        icon.style.transform = originalTransform;
+      });
+
+      clone.addEventListener('animationend', () => {
+        clone.style.transform = originalTransform;
+      });
+    });
+  }
 }
