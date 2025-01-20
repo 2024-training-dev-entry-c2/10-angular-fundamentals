019472ff9e859643ceac53c7ea8f7aeda97618a3
Johan <johanvilla64@gmail.com>
2025-01-20 12:34:28 -0500

    feat: :sparkles: add SlidingBand component with styling

---
+++ src/app/components/sliding-band/sliding-band.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sliding-band',
+  imports: [],
+  templateUrl: './sliding-band.component.html',
+  styleUrl: './sliding-band.component.scss',
+})
+export class SlidingBandComponent {
+  slidingText: string[] = [
+    'Tout commence par un rêve.',
+    'Tout commence par un rêve.',
+    'Tout commence par un rêve.',
+    'Tout commence par un rêve.',
+    'Tout commence par un rêve.',
+    'Tout commence par un rêve.',
+    'Tout commence par un rêve.',
+    'Tout commence par un rêve.',
+  ];
+}
