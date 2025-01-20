19241c7baeb4c5d5e46aa9ebfe4f1aec17de71c6
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2025-01-20 16:56:04 -0300

    fix(main)fix-styles

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { projects } from './projects-data';
 import { MainIntroComponent } from '../main-intro/main-intro.component';
 import { MainProjectComponent } from '../main-project/main-project.component';
@@ -16,52 +16,8 @@ import { MainSeparatorComponent } from '../main-separator/main-separator.compone
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements AfterViewInit {
+export class MainComponent {
   projects = projects;
-
-  ngAfterViewInit(): void {
-    const ctaElements = document.querySelectorAll(
-      '.duties__project-cta, .duties__about-cta'
-    );
-
-    ctaElements.forEach((cta) => {
-      const icon = cta.querySelector(
-        '.duties__project-cta-icon, .duties__about-cta-icon'
-      ) as SVGElement;
-
-      if (icon) {
-        const clone = icon.cloneNode(true) as SVGElement;
-        clone.classList.add('duties__about-cta-icon-clone');
-
-        if (icon.parentNode === cta) {
-          cta.insertBefore(clone, icon);
-        } else {
-          icon.parentElement?.insertBefore(clone, icon.nextSibling);
-        }
-
-        let originalTransform = icon.style.transform;
-
-        cta.addEventListener('mouseenter', () => {
-          originalTransform = icon.style.transform;
-          icon.style.animation = 'moveRight 0.5s forwards';
-          clone.style.animation = 'appear 0.5s forwards';
-        });
-
-        cta.addEventListener('mouseleave', () => {
-          icon.style.animation = 'moveLeft 0.5s forwards';
-          clone.style.animation = 'disappear 0.5s forwards';
-        });
-
-        icon.addEventListener('animationend', () => {
-          icon.style.transform = originalTransform;
-        });
-
-        clone.addEventListener('animationend', () => {
-          clone.style.transform = originalTransform;
-        });
-      }
-    });
-  }
 
   handleProjectClick(title: string) {
     console.log('Project clicked:', title);
