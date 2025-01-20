fc89bbf2781f64e200b3738888b438aea3ac38e5
Johan <johanvilla64@gmail.com>
2025-01-20 12:34:43 -0500

    feat: :sparkles: add SlidingBand component to app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { PromotionBandComponent } from './components/promotion-band/promotion-band.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { SlidingBandComponent } from './components/sliding-band/sliding-band.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PromotionBandComponent, NavbarComponent, HeroComponent],
+  imports: [
+    PromotionBandComponent,
+    NavbarComponent,
+    HeroComponent,
+    SlidingBandComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
