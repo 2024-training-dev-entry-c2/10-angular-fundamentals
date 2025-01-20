89f24f78702b1aa3f9f4b76c479dc3c5e51e4cc1
Johan <johanvilla64@gmail.com>
2025-01-20 11:11:28 -0500

    feat: :sparkles: add Navbar component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PromotionBandComponent } from './components/promotion-band/promotion-band.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PromotionBandComponent],
+  imports: [PromotionBandComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
