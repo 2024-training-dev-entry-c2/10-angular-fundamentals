9dd0b18fcfbae7a749f9e578c5efd58307c10620
Johan <johanvilla64@gmail.com>
2025-01-21 10:05:45 -0500

    refactor: :recycle: separate CollectionsDropdown component with HTML, SCSS

---
+++ src/app/components/collections-dropdown/collections-dropdown.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CollectionsDropdownComponent } from './collections-dropdown.component';
+
+describe('CollectionsDropdownComponent', () => {
+  let component: CollectionsDropdownComponent;
+  let fixture: ComponentFixture<CollectionsDropdownComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CollectionsDropdownComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CollectionsDropdownComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/collections-dropdown/collections-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-dropdown',
+  imports: [],
+  templateUrl: './collections-dropdown.component.html',
+  styleUrl: './collections-dropdown.component.scss'
+})
+export class CollectionsDropdownComponent {
+
+}
