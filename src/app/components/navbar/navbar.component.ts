f820b0f60dc2c5c73ed129e2d2090656e4d090e5
Johan <johanvilla64@gmail.com>
2025-01-21 10:42:02 -0500

    refactor: :recycle: replace SVG cart icon with NavbarCart component for improved modularity

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,10 +3,11 @@ import { NavbarLogotypeComponent } from '../navbar-logotype/navbar-logotype.comp
 import { NavbarIsotypeComponent } from '../navbar-isotype/navbar-isotype.component';
 import { ShopDropdownComponent } from "../shop-dropdown/shop-dropdown.component";
 import { CollectionsDropdownComponent } from "../collections-dropdown/collections-dropdown.component";
+import { NavbarCartComponent } from "../navbar-cart/navbar-cart.component";
 
 @Component({
   selector: 'app-navbar',
-  imports: [NavbarLogotypeComponent, NavbarIsotypeComponent, ShopDropdownComponent, CollectionsDropdownComponent],
+  imports: [NavbarLogotypeComponent, NavbarIsotypeComponent, ShopDropdownComponent, CollectionsDropdownComponent, NavbarCartComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
