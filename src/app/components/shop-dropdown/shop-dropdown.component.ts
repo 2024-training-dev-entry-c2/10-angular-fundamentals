2a6e9fc7fcd01eae7b9dc5ede2008eaadda5ce34
Johan <johanvilla64@gmail.com>
2025-01-21 10:05:22 -0500

    refactor: :recycle: separate ShopDropdown component with HTML, SCSS

---
+++ src/app/components/shop-dropdown/shop-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-dropdown',
+  imports: [],
+  templateUrl: './shop-dropdown.component.html',
+  styleUrl: './shop-dropdown.component.scss'
+})
+export class ShopDropdownComponent {
+
+}
