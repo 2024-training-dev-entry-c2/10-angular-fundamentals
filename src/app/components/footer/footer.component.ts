6c6e2bf8710fa32443011aab1a3debd1845697f7
Johan <johanvilla64@gmail.com>
2025-01-21 11:33:43 -0500

    refactor: :recycle: replace SVG icon with FooterMenuIcon component in footer for improved maintainability

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterMenuIconComponent } from "../footer-menu-icon/footer-menu-icon.component";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  imports: [FooterMenuIconComponent],
 })
 export class FooterComponent {
   menuItems = [
