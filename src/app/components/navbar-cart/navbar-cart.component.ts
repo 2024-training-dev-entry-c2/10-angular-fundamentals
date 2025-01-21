198c2a60932201e59410378b414d57a1d5258230
Johan <johanvilla64@gmail.com>
2025-01-21 10:41:47 -0500

    feat: :sparkles: add NavbarCart component with HTML, SCSS

---
+++ src/app/components/navbar-cart/navbar-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-cart',
+  imports: [],
+  templateUrl: './navbar-cart.component.html',
+  styleUrl: './navbar-cart.component.scss'
+})
+export class NavbarCartComponent {
+
+}
