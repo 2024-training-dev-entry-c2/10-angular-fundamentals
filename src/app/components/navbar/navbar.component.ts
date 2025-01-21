cc6fbd2a2ff00267e46195a4a11d5b86626634ef
Johan <johanvilla64@gmail.com>
2025-01-21 10:05:53 -0500

    refactor: :recycle: integrate ShopDropdown and CollectionsDropdown components into Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component, HostListener } from '@angular/core';
 import { NavbarLogotypeComponent } from '../navbar-logotype/navbar-logotype.component';
 import { NavbarIsotypeComponent } from '../navbar-isotype/navbar-isotype.component';
+import { ShopDropdownComponent } from "../shop-dropdown/shop-dropdown.component";
+import { CollectionsDropdownComponent } from "../collections-dropdown/collections-dropdown.component";
 
 @Component({
   selector: 'app-navbar',
-  imports: [NavbarLogotypeComponent, NavbarIsotypeComponent],
+  imports: [NavbarLogotypeComponent, NavbarIsotypeComponent, ShopDropdownComponent, CollectionsDropdownComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
