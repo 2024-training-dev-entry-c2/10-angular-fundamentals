e8d1690c3e904c24a138eff39553ee12b8d1000a
Matias <alzugaray1997@gmail.com>
2025-01-21 11:55:31 -0300

    fix(development)-dobleFor-getColorDeleted

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,31 +8,40 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  productLinks = [
-    { name: 'Phone (2a) Plus', link: '#' },
-    { name: 'Phone (2a)', link: '#' },
-    { name: 'Phone (2)', link: '#' },
-    { name: 'Ear (open)', link: '#' },
-    { name: 'Ear (a)', link: '#' },
-    { name: 'Ear', link: '#' },
-    { name: 'Accessories', link: '#' },
-    { name: 'Apparel', link: '#' },
-  ];
-
-  companyLinks = [
-    { name: 'About us', link: '#' },
-    { name: 'Careers', link: '#' },
-    { name: 'Community', link: '#' },
-    { name: 'Sustainability', link: '#' },
-    { name: 'Business enquiry', link: '#' },
-    { name: 'Press contact', link: '#' },
-  ];
-
-  supportLinks = [
-    { name: 'Support centre', link: '#' },
-    { name: 'Contact us', link: '#' },
-    { name: 'Find a store', link: '#' },
-    { name: 'Security Vulnerability Report', link: '#' },
+  footerSections = [
+    {
+      title: 'Products',
+      links: [
+        { name: 'Phone (2a) Plus', link: '#' },
+        { name: 'Phone (2a)', link: '#' },
+        { name: 'Phone (2)', link: '#' },
+        { name: 'Ear (open)', link: '#' },
+        { name: 'Ear (a)', link: '#' },
+        { name: 'Ear', link: '#' },
+        { name: 'Accessories', link: '#' },
+        { name: 'Apparel', link: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { name: 'About us', link: '#' },
+        { name: 'Careers', link: '#' },
+        { name: 'Community', link: '#' },
+        { name: 'Sustainability', link: '#' },
+        { name: 'Business enquiry', link: '#' },
+        { name: 'Press contact', link: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { name: 'Support centre', link: '#' },
+        { name: 'Contact us', link: '#' },
+        { name: 'Find a store', link: '#' },
+        { name: 'Security Vulnerability Report', link: '#' },
+      ],
+    },
   ];
 
   legalLinks = [
