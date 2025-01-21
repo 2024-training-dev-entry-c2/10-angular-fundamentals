c6cd7d9135f35aa5cface649a086dd3043f1a184
Johan <johanvilla64@gmail.com>
2025-01-21 07:35:52 -0500

    feat: :sparkles: add additional hero items to HeroMosaicComponent with images and descriptions

--- src/app/components/hero-mosaic/hero-mosaic.component.ts
+++ src/app/components/hero-mosaic/hero-mosaic.component.ts
@@ -36,11 +36,60 @@ export class HeroMosaicComponent {
     },
     {
       imageSrc: 'model_collab.webp',
-      altText:
-        "model showcasing the harry potter cap collab",
+      altText: 'model showcasing the harry potter cap collab',
       text: 'Découvres tous les accessoire de la collab.',
       videoSrc: '',
       link: '#',
     },
+    {
+      imageSrc: 'model_brown_dobby_pull.webp',
+      altText: 'model showcasing the brown dobby pull',
+      text: 'Nouveau pull Dobby Marron.',
+      videoSrc: '',
+      link: '#',
+    },
+    {
+      imageSrc: 'model_full_of_suitcases.webp',
+      altText: 'femenine model showcasing the harry potter t-shirt collection',
+      text: 'Nouveaux BABY TEE.',
+      videoSrc: '',
+      link: '#',
+    },
+    {
+      imageSrc: 'model_back_hagrid_hoodie.webp',
+      altText: "femenine model showcasing the hagrid's themed black hoodie",
+      text: 'Ensemble Hagrid dispo en gris et noir.',
+      videoSrc: '',
+      link: '#',
+    },
+    {
+      imageSrc: 'lookbook_dobby_brown_sock.webp',
+      altText: 'brown dobby sock of free dobby from harry potter',
+      text: 'Libérez Dobby! Voir tous les accessoires.',
+      videoSrc: '',
+      link: '#',
+    },
+    {
+      imageSrc: 'model_vissionaire_harry_potter_t-shirt.webp',
+      altText: 'brown dobby sock of free dobby from harry potter',
+      text: 'Tous les t-shirt de la collab.',
+      videoSrc: '',
+      link: '#',
+    },
+    {
+      imageSrc: '',
+      altText: '',
+      text: '',
+      videoSrc:
+      '//vision-naire.com/cdn/shop/videos/c/vp/c59cfcd5a43c43b6a78721703934eaf2/c59cfcd5a43c43b6a78721703934eaf2.HD-1080p-3.3Mbps-30442736.mp4?v=0',
+      link: '#',
+    },
+    // {
+    //   imageSrc: 'model_vissionaire_harry_potter_t-shirt.webp',
+    //   altText: 'brown dobby sock of free dobby from harry potter',
+    //   text: `<h3>Tous les t-shirt de la collab.</h3>`,
+    //   videoSrc: '',
+    //   link: '#',
+    // },TODO: add support for html in text or highlighted textfield
   ];
 }
