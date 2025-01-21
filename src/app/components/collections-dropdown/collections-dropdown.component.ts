3a6db2d9f5ec9ed503cf958e73fbfb381814526e
Johan <johanvilla64@gmail.com>
2025-01-21 10:24:01 -0500

    refactor: :recycle: update CollectionsDropdown component to use dynamic item lists for improved maintainability and reduce repeated code

--- src/app/components/collections-dropdown/collections-dropdown.component.ts
+++ src/app/components/collections-dropdown/collections-dropdown.component.ts
@@ -4,8 +4,23 @@ import { Component } from '@angular/core';
   selector: 'app-collections-dropdown',
   imports: [],
   templateUrl: './collections-dropdown.component.html',
-  styleUrl: './collections-dropdown.component.scss'
+  styleUrl: './collections-dropdown.component.scss',
 })
 export class CollectionsDropdownComponent {
-
+  firstColumn = [
+    'HARRY POTTER | VISIONNAIRE',
+    'Lookbook N°3',
+    'Lookbook N°4',
+    'Lookbook N°5',
+    'Les couleurs',
+    'Collab Jordy VDN',
+  ];
+  secondColumn = [
+    'COLLAB LE COQ SPORTIF',
+    'Lookbook N°6',
+    'Lookbook 6.1',
+    'Lookbook N°7',
+    'Lookbook N°7.1',
+    'Lookbook N°7.2',
+  ];
 }
